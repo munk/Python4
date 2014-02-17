03fe662086a9468f597a72f871919ac6ba690f18
Munk <john.downs@acm.org>
2014-02-16 23:20:55 -0500

    Adding root action to app

--- app.py
+++ app.py
@@ -1,21 +1,35 @@
 from flask import Flask
-app = Flask(__name__)
+import json
+import sqlite3
 
-from yelp import phone_search
-from inspections import inspection_data
+app = Flask(__name__)
+db = sqlite3.connect('noms.db', check_same_thread=False)
+
+def query(zipcode):
+    q = "SELECT * FROM NEIGHBORHOOD WHERE ZIPCODE = ?"
+    cur = db.cursor()
+    cur.execute(q, [zipcode])
+    result = cur.fetchone()
+    try:
+        return {'zipcode': result[0], 'grade': result[1], 'rating': result[2]}
+    except:
+        return {'error': 'Unable to find zipcode %s' % zipcode}
+
+def all_zipcodes():
+    q = "SELECT ZIPCODE FROM NEIGHBORHOOD"
+    cur = db.cursor()
+    cur.execute(q)
+    return [code[0] for code in cur.fetchall()]
 
 @app.route("/")
-def hello():
-    return "Hello World!"
-
-def foo():
-    data = inspection_data('manhattan', 10)
-    for d in data:
-        for phone in d[1].PHONE:
-            result = phone_search(phone)
-            print result.text
-            print result.url
+def home():
+    return json.dumps(all_zipcodes())
 
 
-if __name__ == "__main__":
-    foo()
+@app.route("/<zipcode>")
+def zipdata(zipcode):
+    response = query(zipcode)
+    return json.dumps(response)
+
+if __name__ == '__main__':
+    app.run(debug=True)
